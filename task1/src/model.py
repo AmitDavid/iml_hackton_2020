d60c751d22b403e23a79ad2fdaeda7ebea389658
amitDavid <amit1dv6@gmail.com>
2020-06-10 21:36:04 +0300

    add git igonre to data folder

--- task1/src/model.py
+++ task1/src/model.py
@@ -2,8 +2,13 @@ import numpy as np
 import pandas as pd
 import sklearn as sk
 
+PATH_TO_TRAIN_DATA = "../data/train_data.csv"
+PATH_TO_TEST_DATA = "../data/train_data.csv"
+PATH_TO_WEATHER_DATA = "../data/all_weather_data.csv"
+
+
 class FlightPredictor:
-    def __init__(self, path_to_weather=''):
+    def __init__(self, path_to_data='', path_to_weather=''):
         """
         Initialize an object from this class.
         @param path_to_weather: The path to a csv file containing weather data.
@@ -21,4 +26,5 @@ class FlightPredictor:
 
 
 if __name__ == '__main__':
-    pass
+    ml = FlightPredictor(PATH_TO_TRAIN_DATA, PATH_TO_WEATHER_DATA)
+    # y_hat = ml.predict(PATH_TO_TEST_DATA)
