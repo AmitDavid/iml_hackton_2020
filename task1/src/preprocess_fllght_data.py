ab3fde61e52af87a5bbda7078e4b14632e4749ff
amitDavid <amit1dv6@gmail.com>
2020-06-11 11:02:06 +0300

    add split data to train and test function

--- task1/src/preprocess_fllght_data.py
+++ task1/src/preprocess_fllght_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def preprocess_flight_data(df: pd.DataFrame):
-    '''
+    """
     :param df:  Pandas DataFrame contain the following:
                 DayOfWeek:	                     Day of Week
                 FlightDate:	                     Flight Date (yyyy-mm-dd)
@@ -22,8 +22,8 @@ def preprocess_flight_data(df: pd.DataFrame):
 
                 ArrDelay:                        Time difference (in minutes) from expected arrival time
                 DelayFactor:                     Type fo delay
-    :return: Processed data frame.
-    '''
+    :return: X - Processed data frame. y_delay - Time difference , y_type - Type fo delay
+    """
     # TODO: might make them dummies as well, check if make prediction better
     # Remove OriginCityName, OriginState, DestCityName, DestState,
     del df['OriginCityName']
@@ -31,13 +31,14 @@ def preprocess_flight_data(df: pd.DataFrame):
     del df['DestCityName']
     del df['DestState']
 
-    # Get categorical features (dummies) for dayOfTheWeek, Reporting_Airline,
+    # Get categorical features (dummies) for dayOfTheWeek, Reporting_Airline, Tail_Number
     # Flight_Number_Reporting_Airline, Origin, Dest
     df = pd.get_dummies(df, columns=['DayOfWeek', 'Reporting_Airline', 'Tail_Number',
                                      'Flight_Number_Reporting_Airline', 'Origin', 'Dest'])
 
-    # Get hour and ten of mintues of CRSDepTime and CRSArrTime
-    # Tf flight was at 1546.0 (15:46), save it to 154 dummy
+    # Get hour and ten of minutes of CRSDepTime and CRSArrTime
+    # If flight was at 1546.0 (15:46), save it to 154 dummy
+    # If flight was at 133.0 (01:33), save it to 013 dummy (with leading zero)
     df['CRSDepTime'] = df['CRSDepTime'].str.slice(stop=-3).str.zfill(3)
     df['CRSArrTime'] = df['CRSArrTime'].str.slice(stop=-3).str.zfill(3)
     df = pd.get_dummies(df, columns=['CRSDepTime'])
@@ -55,21 +56,38 @@ def preprocess_flight_data(df: pd.DataFrame):
     del df['FlightDate']
 
     # split ArrDelay and DelayFactor to results DataFrame
-    y = pd.DataFrame()
-    y['ArrDelay'] = df['ArrDelay']
-    y['DelayFactor'] = df['DelayFactor']
+    y_delay = df['ArrDelay']
     del df['ArrDelay']
+
+    y_type = df['DelayFactor']
     del df['DelayFactor']
 
     # CRSElapsedTime and Distance left unchanged
-    return df, y
+    return df, y_delay, y_type
+
+
+def split_to_train_test(df, y_1, y_2, ratio=5):
+    """
+    Split data to train and test sets
+    :param df:
+    :param y_1:
+    :param y_2:
+    :param ratio:
+    :return:
+    """
+    cut = int(len(df) / ratio)
+    return df[cut:], y_1[cut:], y_2[cut:], df[:cut], y_1[:cut],  y_2[:cut]
 
 
 if __name__ == '__main__':
     df = pd.read_csv("../data/data1k.csv", dtype={'FlightDate': str, 'CRSDepTime': str,
                                                   'CRSArrTime': str})
-    X, y = preprocess_flight_data(df)
+
+    X, y_delay, y_type = preprocess_flight_data(df)
     X.to_csv("results_X.csv")
-    y.to_csv("results_y.csv")
+
+    train_X, train_y_1, train_y_2, test_X, test_y_1, test_y_2 = split_to_train_test(X, y_delay, y_type)
+    print(len(train_X), len(train_y_1), len(train_y_2), len(test_X), len(test_y_1), len(test_y_2))
+
     print('done')
 
