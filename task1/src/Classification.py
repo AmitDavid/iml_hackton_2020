7ff731b57390c9c0b59d1d7043722584aba71cc0
Rachely Butterman <rbutterm@cs.huji.ac.il>
2020-06-11 16:32:10 +0300

    Add files via upload

--- task1/src/Classification.py
+++ task1/src/Classification.py
@@ -11,56 +11,62 @@ from preprocess_fllght_data import preprocess_flight_data
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 
-def score(model,X_test ,y_test):
+def score(model,X_test ,y_test,model_name):
     y_pred = model.predict(X_test)
     acc=accuracy_score(y_test, y_pred)
     pre=precision_score(y_test, y_pred, average='weighted')
     rec=recall_score(y_test, y_pred, average='weighted')
     f1=f1_score(y_test, y_pred, average='weighted')
-    return pd.DataFrame({'accuracy':acc,'precision':pre,'recall':rec,'f1':f1}, index=[0])
+    return pd.DataFrame({'model_name':model_name,'accuracy':acc,'precision':pre,'recall':rec,'f1':f1}, index=[0])
 
 def SVM(X_train,y_train,X_test,y_test):
-    svm = SVC(C=1, kernel='linear',decision_function_shape='ovo')
+    svm = SVC(C=1e6, kernel='linear',decision_function_shape='ovo')
     svm.fit(X_train,y_train)
     s=score(svm,X_test, y_test)
     return s
 
 def Logistic(X_train,y_train,X_test,y_test):
-    logistic = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter = 20)
+    model_name='Logistic'
+    logistic = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter = 80)
     logistic.fit(X_train,y_train)
-    s=score(logistic,X_test, y_test)
+    s=score(logistic,X_test, y_test,model_name)
     return s
 
 # 7. e.
 def DecisionTree(X_train,y_train,X_test,y_test):
+    model_name='DecisionTree'
     tree = DecisionTreeClassifier(min_samples_split=0.1)
     tree.fit(X_train,y_train)
-    s=score(tree,X_test, y_test)
+    s=score(tree,X_test, y_test,model_name)
     return s
 
 def RandomForest(X_train, y_train, X_test, y_test):
-    randtree = RandomForestClassifier(n_estimators=100,max_depth=10, random_state=0)
+    model_name='RandomForest'
+    randtree = RandomForestClassifier(n_estimators=100,min_samples_split=0.1, random_state=0)
     randtree.fit(X_train,y_train)
-    s=score(randtree,X_test, y_test)
+    s=score(randtree,X_test, y_test,model_name)
     return s
 
 def  Soft_SVM(X_train,y_train,X_test,y_test):
-    s_svm = SVC(C=1e-2, kernel='linear',decision_function_shape='ovo')
+    model_name='Soft_SVM'
+    s_svm = SVC(C=1e-2, kernel='poly',decision_function_shape='ovr')
     s_svm.fit(X_train,y_train)
-    s=score(s_svm,X_test, y_test)
+    s=score(s_svm,X_test, y_test,model_name)
     return s
 
 
 def k_nearest_neighbors(X_train,y_train,X_test,y_test):
-    knn = KNeighborsClassifier(n_neighbors=5)
+    model_name='k_nearest_neighbors'
+    knn = KNeighborsClassifier(n_neighbors=20)
     knn.fit(X_train,y_train)
-    s=score(knn,X_test, y_test)
+    s=score(knn,X_test, y_test,model_name)
     return s
 
 def nn(X_train,y_train,X_test,y_test):
-    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes = (5, 2), random_state = 1)
+    model_name='neural_network'
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, random_state = 1)
     clf.fit(X_train,y_train)
-    s=score(clf,X_test, y_test)
+    s=score(clf,X_test, y_test,model_name)
     return s
 
 
@@ -73,11 +79,11 @@ if __name__ == "__main__":
     y_type=y_type.drop(listNAN)
     X_train, X_test, y_train, y_test = train_test_split(X, y_type, test_size=0.2)
     #dfs=[]
-    # s=SVM(X_train,y_train,X_test,y_test)
-    dfs = Logistic(X_train, y_train, X_test, y_test)
+    #dfs=SVM(X_train,y_train,X_test,y_test)
+    dfs =Logistic(X_train, y_train, X_test, y_test)
     dfs=dfs.append(DecisionTree(X_train, y_train, X_test, y_test),ignore_index=True)
     dfs=dfs.append(RandomForest(X_train, y_train, X_test, y_test),ignore_index=True)
-    # dfs=dfs.append(Soft_SVM(X_train, y_train, X_test, y_test))
+    dfs=dfs.append(Soft_SVM(X_train, y_train, X_test, y_test))
     dfs=dfs.append(k_nearest_neighbors(X_train, y_train, X_test, y_test),ignore_index=True)
     dfs=dfs.append(nn(X_train, y_train, X_test, y_test),ignore_index=True)
     o=0
